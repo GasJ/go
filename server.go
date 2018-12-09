bc243830c970c26013149c30524d4bad260f8d4a
gasjet <ansleunawxj@live.cn>
2018-12-08 21:48:33 -0600

    wwe

--- server.go
+++ server.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	//"database/sql"
 	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 	"net/http"
 )
@@ -39,23 +36,35 @@ func signing(w http.ResponseWriter, r * http.Request)  {
 		return
 	}
 
-	rows, err := db.Query("SHOW DATABASES")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Could not query db: %v", err), 500)
+	table, err := db.Query("SHOW TABLES")
+
+	if err == nil {
+		w.Write([]byte("sorry, cannot"))
+		println("we cannot get in users")
 		return
 	}
-	defer rows.Close()
 
-	buf := bytes.NewBufferString("Databases:\n")
-	for rows.Next() {
-		var dbName string
-		if err := rows.Scan(&dbName); err != nil {
-			http.Error(w, fmt.Sprintf("Could not scan result: %v", err), 500)
-			return
-		}
-		fmt.Fprintf(buf, "- %s\n", dbName)
-	}
-	w.Write(buf.Bytes())
+	var dbName string
+	table.Scan(dbName)
+	w.Write([]byte(dbName))
+	
+	//rows, err := db.Query("SHOW DATABASES")
+	//if err != nil {
+	//	http.Error(w, fmt.Sprintf("Could not query db: %v", err), 500)
+	//	return
+	//}
+	//defer rows.Close()
+	//
+	//buf := bytes.NewBufferString("Databases:\n")
+	//for rows.Next() {
+	//	var dbName string
+	//	if err := rows.Scan(&dbName); err != nil {
+	//		http.Error(w, fmt.Sprintf("Could not scan result: %v", err), 500)
+	//		return
+	//	}
+	//	fmt.Fprintf(buf, "- %s\n", dbName)
+	//}
+	//w.Write(buf.Bytes())
 
 
 
@@ -66,7 +75,6 @@ func signing(w http.ResponseWriter, r * http.Request)  {
 	//println("cnm...")
 	//w.Write([]byte("linked..."))
 	//db, err := sql.Open("firstnote", "root:root@/uestcbook")
-	//
 	//result, err := db.Exec(
 	//	"INSERT INTO users (name, age) VALUES (?, ?)",
 	//	"gopher",
